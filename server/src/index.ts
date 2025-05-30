efc81b22c25c7b4f8e5a8e1defa0086d37052ee4
artndev <97910780+artndev@users.noreply.github.com>
2025-05-30 12:31:58 +0300

    .

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,25 +2,22 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 import path from 'path'
-const clientBuildPath = path.join(process.cwd(), 'client', 'dist')
-
 import express from 'express'
 import './bot.js' // bot is loaded here
 import config from './config.json' with { type: 'json' }
 import { usersController } from './controllers/_controllers.js'
 import * as routers from './routers/_routers.js'
-// import cors from 'cors'
+import cors from 'cors'
 
 const app = express()
-// app.use(
-//   cors({
-//     origin: config.CLIENT_URL,
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     credentials: true,
-//   })
-// )
+app.use(
+  cors({
+    origin: config.CLIENT_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  })
+)
 app.use(express.json())
-app.use(express.static(clientBuildPath))
 
 app.use('/api/products', routers.productsRouter)
 app.use('/api/orders', routers.ordersRouter)
@@ -33,10 +30,6 @@ app.post('/api/login', async (req, res) => {
     answer: true,
   })
 }) // ?id=
-
-app.get('/*', (_req, res) => {
-  res.sendFile(path.join(clientBuildPath, 'index.html'))
-})
 
 const port = config.SERVER_PORT
 app.listen(port, () =>
